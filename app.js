3f775dcd30efe72f705d0fdc42ccd84489c51dbe
Soulbroda23 <ogundeyi.dieko13@gmail.com>
2023-09-23 11:57:20 -0600

    written necessary function to  display info to the DOM

--- app.js
+++ app.js
@@ -14,22 +14,37 @@ async function fetchWeatherApi(){
   const City = document.querySelector(".city");
   const time = document.querySelector(".time__date");
   const weatherCondition = document.querySelector(".weather__condition");
-  const region = document.querySelector(".region");
+  const weatherImage = document.querySelector(".weather__image")
   const searchInput = document.querySelector(".search__city");
   const cityName = searchInput.value;
-
+  const toggleSwitch = document.querySelector(".toggle");
+  const text = document.querySelector(".text");
+  const cloud = document.querySelector(".cloud")
 
   try{
   let myApiKey = '2471772679344b31b0d121026232309';
   let myApiUrl =` http://api.weatherapi.com/v1/current.json?key=${myApiKey}&q=${cityName}`;
+  
   //fetch the API using try 
-  let getWeather = await fetch (myApiUrl);
+  let getWeather = await fetch (myApiUrl, {
+    method: 'GET', 
+    mode: 'cors',});
+  
   //if fetching API was successful , do the following;
   if(getWeather.ok){
   let data = await getWeather.json();
-  City.textContent = data.location.name
-  console.log(data.location);
-  
+  City.textContent = `${data.location.name}, ${data.location.region}`;
+  time.textContent = data.location.localtime;
+  weatherImage.src = data.current.condition.icon;
+  text.textContent = data.current.condition.text;
+  cloud.textContent = data.current.cloud;
+  console.log(data.current.condition)
+  if(toggleSwitch.checked){
+    `${weatherCondition.textContent = data.current.temp_c}C`;
+  }
+  else{
+    `${weatherCondition.textContent = data.current.temp_f}F`;
+  }
   }
 }
   catch{
